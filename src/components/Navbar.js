a847dba4000fbc4743ef4666a3c22c394787cb5a
Anandateertha <www.anandateertha@gmail.com>
2023-07-20 12:29:27 +0530

    Background and Font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../styles/Navbar.css'
 
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-primary">
-                <div className="container-fluid">
+                <div className="container-fluid text">
+                    <img src="./apple-icon.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
                     <a className="navbar-brand" href="/">Calculator</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+
+
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
